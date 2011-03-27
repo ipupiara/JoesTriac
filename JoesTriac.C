f3686107e992858ceeefe0955d1f4973c6935737
unknown <duenda@.(none)>
2011-03-27 19:26:46 +0200

    first entry

---
+++ JoesTriac.C
@@ -0,0 +1,36 @@
+
+#include <REG52.H>                /* special function register declarations   */
+                                  /* for the intended 8051 derivative         */
+
+#include <stdio.h>                /* prototype declarations for I/O functions */
+
+
+#ifdef MONITOR51                         /* Debugging with Monitor-51 needs   */
+char code reserve [3] _at_ 0x23;         /* space for serial interrupt if     */
+#endif                                   /* Stop Exection with Serial Intr.   */
+                                         /* is enabled                        */
+
+
+
+void main (void) {
+
+/*------------------------------------------------
+Setup the serial port for 1200 baud at 16MHz.
+------------------------------------------------*/
+#ifndef MONITOR51
+    SCON  = 0x50;		        /* SCON: mode 1, 8-bit UART, enable rcvr      */
+    TMOD |= 0x20;               /* TMOD: timer 1, mode 2, 8-bit reload        */
+    TH1   = 221;                /* TH1:  reload value for 1200 baud @ 16MHz   */
+    TR1   = 1;                  /* TR1:  timer 1 run                          */
+    TI    = 1;                  /* TI:   set TI to send first char of UART    */
+#endif
+
+
+  while (1) {
+    P1 ^= 0x01;
+	P2 = 0x00;    		    
+    printf ("Hello World\n");   
+  }
+}
+
+
