aade9c87033279715f0d63bb691ff216142f1b71
unknown <duenda@.(none)>
2011-04-18 14:39:45 +0200

    next attempt

---
+++ joesTriac.c
@@ -0,0 +1,16 @@
+/*
+ * joesTriac.c
+ *
+ * Created: 18.04.2011 14:34:59
+ *  Author: duenda
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
